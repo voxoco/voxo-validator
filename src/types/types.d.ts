928ede310e3d3b39b7022e6877a0b99c4991a3c5
Nathanael McDaniel <leemcd56@gmail.com>
2020-08-07 11:59:17 -0500

    Sanitizer spec now tests every condition

--- src/types/types.d.ts
+++
@@ -1,3 +0,0 @@
-declare type Ruleset = {
-  [key: string]: string[]
-}
--- tests/sanitizer.spec.ts
+++ tests/sanitizer.spec.ts
@@ -1,6 +1,91 @@
 import * as validator from '../src/index';
 
 describe('sanitizer', () => {
+  it('can escape strings', () => {
+    const data: any = { name: 'Sears & Roebuck' };
+
+    const schema = {
+      name: 'escape',
+    };
+
+    validator.sanitize(data, schema);
+
+    expect(data).toMatchObject({
+      name: 'Sears &amp; Roebuck',
+    });
+  });
+
+  it('can transform strings to lowercase', () => {
+    const data: any = { name: 'VOXO_API' };
+
+    const schema = {
+      name: 'lower_case',
+    };
+
+    validator.sanitize(data, schema);
+
+    expect(data).toMatchObject({
+      name: 'voxo_api',
+    });
+  });
+
+  it('can pluralize strings', () => {
+    const data: any = { fruit: 'apple' };
+
+    const schema = {
+      fruit: 'plural',
+    };
+
+    validator.sanitize(data, schema);
+
+    expect(data).toMatchObject({
+      fruit: 'apples',
+    });
+  });
+
+  it('can singularize strings', () => {
+    const data: any = { fruit: 'apples' };
+
+    const schema = {
+      fruit: 'singular',
+    };
+
+    validator.sanitize(data, schema);
+
+    expect(data).toMatchObject({
+      fruit: 'apple',
+    });
+  });
+
+  it('can convert strings to slugs', () => {
+    const data: any = { name: 'Wes Anderson' };
+
+    const schema = {
+      name: 'slug',
+    };
+
+    validator.sanitize(data, schema);
+
+    expect(data).toMatchObject({
+      name: 'wes-anderson',
+    });
+  });
+
+  it('can strip links', () => {
+    const data: any = { description: 'Click <a href="#">here</a>' };
+
+    const schema = {
+      description: 'strip_links',
+    };
+
+    validator.sanitize(data, schema);
+
+    expect(data).toMatchObject({
+      description: 'Click here',
+    });
+  });
+
+
   it('can strip unsafe HTML', () => {
     const data: any = { name: '<p>Wes Anderson</p>' };
 
@@ -12,6 +97,20 @@ describe('sanitizer', () => {
 
     expect(data).toMatchObject({
       name: 'Wes Anderson',
+    });
+  });
+
+  it('can transform strings to uppercase', () => {
+    const data: any = { name: 'voxo_api' };
+
+    const schema = {
+      name: 'upper_case',
+    };
+
+    validator.sanitize(data, schema);
+
+    expect(data).toMatchObject({
+      name: 'VOXO_API',
     });
   });
 });
